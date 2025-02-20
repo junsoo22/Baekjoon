44e6147d9f5f53f4601b8eb1c567d05ea437068e
Junsoo Kim <165903307+junsoo22@users.noreply.github.com>
2025-02-20 16:34:03 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,16 @@
+a,b,c=map(int,input().split())
+
+price=0
+
+if a==b==c:
+    price=10000+a*1000
+elif a==b:
+    price=1000+a*100
+elif b==c:
+    price=1000+b*100
+elif a==c:
+    price=1000+c*100
+else:
+    price=max(a,b,c)*100
+
+print(price)
