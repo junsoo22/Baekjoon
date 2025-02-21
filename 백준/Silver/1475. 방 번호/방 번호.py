17875987e42216120eaf2d5577a7f04a6bdb10c4
Junsoo Kim <165903307+junsoo22@users.noreply.github.com>
2025-02-21 13:30:52 +0900

    [Silver V] Title: 방 번호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=[0]*10
+s=str(n)
+for i in s:
+
+    if int(i)==6 or int(i)==9:
+        if arr[6]<=arr[9]:
+            arr[6]+=1
+        else:
+            arr[9]+=1
+    else:
+        arr[int(i)]+=1
+
+print(max(arr))
