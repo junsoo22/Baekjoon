e5c882ca543333e06f4377edf67d8d626b691234
Junsoo Kim <165903307+junsoo22@users.noreply.github.com>
2025-11-11 14:07:08 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 5일차 - 배열 최소 합, Time: 115 ms, Memory: 60,544 KB -BaekjoonHub

--- SWEA/D2/4881. ［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합/［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합.py
+++ SWEA/D2/4881. ［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합/［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합.py
@@ -1,28 +1,29 @@
-
-def dfs(idx,sm):   #n 행번호, j: 열번호
+def dfs(idx,sm):
     global ans
+    
+    #가지치기
     if ans<=sm:
         return
-
     
-    #종료조건
     if idx==n:
         ans=min(ans,sm)
         return
-    for j in range(len(arr)):
-        if visited[j]==0:
-            visited[j]=1
-            dfs(idx+1, sm+arr[idx][j])
-            visited[j]=0
 
-tc=int(input())
-for t in range(tc):
+    for i in range(n):
+        if visited[i]==0:
+            visited[i]=1
+            dfs(idx+1,sm+arr[idx][i])
+            visited[i]=0
+
+t=int(input())
+
+for tc in range(t):
     n=int(input())
     arr=[]
     for i in range(n):
         arr.append(list(map(int,input().split())))
     ans=n*10
-    visited=[0]*n
+    visited=[0 for i in range(n)]
     dfs(0,0)
-        
-    print(f"#{t+1} {ans}")
+    print(f"#{tc+1} {ans}")
+
