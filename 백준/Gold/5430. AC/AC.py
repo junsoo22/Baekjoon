dd999b85bbad95c85d390c63de62bf16c4627578
Junsoo Kim <165903307+junsoo22@users.noreply.github.com>
2026-03-11 20:55:10 +0900

    [Gold V] Title: AC, Time: 272 ms, Memory: 39992 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -4,32 +4,36 @@ import sys
 input=sys.stdin.readline
 
 t=int(input())
+
 for i in range(t):
-    queue=deque()
     p=input()
     n=int(input())
-    arr=input().strip()
+    arr=input().rstrip()
+
+    queue=deque()
+    result=""
+    reverse=False
     if n==0:
         queue=[]
     else:
         queue=deque(map(int,arr[1:-1].split(',')))
-    flag=0
-    reverse=0
-    for i in p:
-        if i=="R":
+    for j in range(len(p)):
+        if p[j]=='R':
             reverse=not reverse
 
-        elif i=="D":
+        elif p[j]=='D':
             if len(queue)==0:
-                print("error")
-                flag=1
+                result="error"
                 break
+            if reverse:
+                queue.pop()
             else:
-                if reverse:
-                    queue.pop()
-                else:
-                    queue.popleft() 
-    if not flag:
+                queue.popleft()
+
+    if result:
+
+        print(result)
+    else:
         if reverse:
             queue.reverse()
         print("[" + ",".join(map(str, queue)) + "]")
