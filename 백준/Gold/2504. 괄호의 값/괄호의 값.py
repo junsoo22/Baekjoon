b365356aa8ec4e19ebb6b28bc4c0bc62b3f8aff8
Junsoo Kim <165903307+junsoo22@users.noreply.github.com>
2026-03-16 18:30:21 +0900

    [Gold V] Title: 괄호의 값, Time: 64 ms, Memory: 34936 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+str=input()
+
+queue=deque()
+mul=1
+result=0
+for i in range(len(str)):
+    if str[i] == '(':
+        queue.append('(')
+        mul *= 2
+
+    elif str[i] == '[':
+        queue.append('[')
+        mul *= 3
+            
+    elif str[i]==')':
+        if not queue or queue[-1]!='(':
+            print(0)
+            exit()
+        if str[i-1]=='(':
+            result+=mul
+
+        queue.pop()
+        mul//=2
+
+    elif str[i]==']':
+        if not queue or queue[-1]!='[':
+            print(0)
+            exit()
+        if str[i-1]=='[':
+            result+=mul
+
+        queue.pop()
+        mul//=3
+if queue:
+    print(0)
+else:
+    print(result)
