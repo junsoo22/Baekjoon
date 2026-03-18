bef816f11ca8664e3e64748a0ec01e558127dd9c
Junsoo Kim <165903307+junsoo22@users.noreply.github.com>
2026-03-18 18:30:17 +0900

    [Silver I] Title: 스타트와 링크, Time: 1036 ms, Memory: 306044 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,53 @@
+from itertools import combinations, permutations
+import sys
+
+input=sys.stdin.readline
+
+n=int(input())
+nums=[i for i in range(1,n+1)]
+combi = list(combinations(nums, n//2))
+
+
+skill=[]
+s=[]
+l=[]
+nums=set(nums)
+
+for i in combi:
+    i=set(i)
+    s.append(i)
+    l.append(nums-i)
+
+for i in range(n):
+    skill.append(list(map(int,input().split())))
+
+result=[]
+sumA=0
+sumB=0
+answer=0
+for i in range(len(s)):
+
+    a=list(s[i])
+    b=list(l[i])
+
+    sumA=0
+    sumB=0
+    for j in a:
+        for k in a:
+            sumA+=skill[j-1][k-1]
+
+
+    for j in b:
+        for k in b:
+            sumB+=skill[j-1][k-1]
+
+
+    answer=abs(sumA-sumB)
+    result.append(answer)
+print(min(result))
+
+
+
+
+
+
