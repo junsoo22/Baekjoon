a096f01c0dde7dec3ddbc08d5538b56641fd0f6b
Junsoo Kim <165903307+junsoo22@users.noreply.github.com>
2026-05-18 22:35:35 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 91 ms, Memory: 60,288 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.py
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.py
@@ -1,30 +1,30 @@
-for tc in range(1, 11):
-    n = int(input())  # 원본 암호문 길이
-    arr = list(map(int, input().split()))  # 암호문 리스트
 
-    k = int(input())  # 명령어 개수
-    commands = input().split()  # 모든 명령을 한 줄로 받음
-    idx=0
-    for i in range(k):
-        cmd=commands[idx]
-        idx+=1
-        if cmd=='I':  #삽입
-            x=int(commands[idx])
-            y=int(commands[idx+1])
-            idx+=2
-            s=list(map(int, commands[idx:idx+y]))
-            idx+=y
-            arr=arr[:x]+s+arr[x:]  # x 위치에 s 삽입
-        elif cmd=='D':  #삭제
-            x=int(commands[idx])
-            y=int(commands[idx+1])
-            idx+=2
-            arr=arr[:x]+arr[x+y:]  # x부터 y개 삭제
-        elif cmd=='A':  #추가
-            y=int(commands[idx])
-            idx+=1
-            s=list(map(int, commands[idx:idx+y]))
-            idx+=y
-            arr=arr+s  # 맨 뒤에 s 추가
-    
-    print(f"#{tc}", *arr[:10])  
+
+for tc in range(10):
+    N = int(input())
+    code = list(map(int, input().split()))
+    order_num = int(input())
+    order = input().split()
+
+    idx = 0
+    while idx < len(order):
+        if order[idx] == 'I':
+            x = int(order[idx + 1])
+            y = int(order[idx + 2])
+            s = list(map(int, order[idx + 3:idx + 3 + y]))
+            code[x:x] = s
+            idx += y + 3
+        elif order[idx] == 'D':
+            x = int(order[idx + 1])
+            y = int(order[idx + 2])
+
+            del code[x:x+y]
+            idx += 3
+        elif order[idx] == 'A':
+            y = int(order[idx + 1])
+            s = list(map(int, order[idx + 2: idx + 2 + y]))
+            code[-1:-1] = s
+            idx += y + 2
+
+    print(f"#{tc+1} ", *code[:10])
+
