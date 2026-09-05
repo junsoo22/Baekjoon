a8483adb68a839ff19f3f178975c5ef485174e32
Junsoo Kim <165903307+junsoo22@users.noreply.github.com>
2026-09-05 16:11:14 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,38 +1,32 @@
 from collections import deque
 
-def can_convert(a,b):   #두 단어가 한 글자만 다르면 True
-    diff=0
+def can_convert(a, b):
+    diff = 0
+
     for i in range(len(a)):
-        if a[i]!=b[i]:
-            diff+=1
-    return diff==1
+        if a[i] != b[i]:
+            diff += 1
+
+    return diff == 1
+
 
 def solution(begin, target, words):
-    answer = 0
-    cnt=0
-    
 
-    
-    queue=deque()
-    queue.append((begin,0))
-    
-    visited=set()
-    
     if target not in words:
         return 0
-    
+
+    queue = deque([(begin, 0)])
+    visited = set()
+
     while queue:
-        word,cnt=queue.popleft()
-        
-        if word==target:
+        word, cnt = queue.popleft()
+
+        if word == target:
             return cnt
-        
+
         for w in words:
-            if w not in visited and can_convert(word,w):
+            if w not in visited and can_convert(word, w):
                 visited.add(w)
-                queue.append((w,cnt+1))
-                print(queue)
-                print(visited)
-    
-        
+                queue.append((w, cnt + 1))
+
     return 0
